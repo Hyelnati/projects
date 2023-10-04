245dadb901e62ee40aa7fd626cfec71cefab43cc
Hyelnati D. Yerima <hyelnati@gmail.com>
2023-10-04 10:36:22 +0100

    update

---
+++ src/test/java/africa/semicolon/gemstube/services/MailServiceTest.java
@@ -0,0 +1,13 @@
+package africa.semicolon.gemstube.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MailServiceTest {
+
+    @Test
+    public void testSendEmail(){
+
+    }
+}
